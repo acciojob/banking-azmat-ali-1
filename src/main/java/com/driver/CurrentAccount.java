85d085041aec98274163ae8a33676283edb621ef
AZMAT ALI <222azmat@gmail.com>
2022-12-30 17:41:16 +0530

    Test 1

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -3,6 +3,14 @@ package com.driver;
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
+    }
+
+    public void setTradeLicenseId(String tradeLicenseId) {
+        this.tradeLicenseId = tradeLicenseId;
+    }
+
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
         super(name,balance,5000);
@@ -12,7 +20,7 @@ public class CurrentAccount extends BankAccount{
         this.tradeLicenseId=tradeLicenseId;
     }
 
-    public boolean valid(String S){
+    public String valid(String S){
         int[] hash = new int[26];
         for (int i = 0; i < S.length(); i++) {
             hash[S.charAt(i) - 'a']++;
@@ -25,9 +33,26 @@ public class CurrentAccount extends BankAccount{
             }
         }
         if (max > (S.length() + 1) / 2) {
-            return false;
+            return "";
         }
-        return true;
+        char[] res = new char[S.length()];
+        int idx = 0;
+        while (hash[letter] > 0) {
+            res[idx] = (char) (letter + 'a');
+            idx += 2;
+            hash[letter]--;
+        }
+        for (int i = 0; i < hash.length; i++) {
+            while (hash[i] > 0) {
+                if (idx >= res.length) {
+                    idx = 1;
+                }
+                res[idx] = (char) (i + 'a');
+                idx += 2;
+                hash[i]--;
+            }
+        }
+        return String.valueOf(res);
     }
     public void validateLicenseId() throws Exception {
         // A trade license Id is said to be valid if no two consecutive characters are same
@@ -36,8 +61,12 @@ public class CurrentAccount extends BankAccount{
         // If it is not possible, throw "Valid License can not be generated" Exception
         for(int i=0;i<tradeLicenseId.length()-1;i++){
             if(tradeLicenseId.charAt(i)==tradeLicenseId.charAt(i+1)){
-                if(valid(tradeLicenseId)==false){
+                String s = valid(tradeLicenseId);
+                if(s.equals("")){
                     throw new Exception("Valid License can not be generated");
+                }
+                else {
+                    tradeLicenseId=s;
                 }
                 break;
             }
