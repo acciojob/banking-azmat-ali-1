1f55ab409bc526041ffd12684d0b7330762a909a
AZMAT ALI <222azmat@gmail.com>
2022-12-30 17:31:57 +0530

    Test 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,5 @@ package com.driver;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        SavingsAccount s = new SavingsAccount("Azmat",500,500,0.30);
-        System.out.println(s.getSimpleInterest(10));
-      s.withdraw(600);
     }
 }
